206254ef7b0986dc9ad3e8848b44cf5025006438
Thippeswammy <thippeswamy636408@gmail.com>
2026-05-12 20:33:36 +0530

    fix: update cmd_vel remappings to unstamped topic and set Rviz2 map reliability to best effort

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
             package="twist_mux",
             executable="twist_mux",
             parameters=[twist_mux_params, {'use_sim_time': True}],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel')]
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
         )
 
     gazebo_params_file = os.path.join(
